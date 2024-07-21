3ca00af8f845b601e1a34af72747143ec73f8dba
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-07-20 22:47:38 -0300

    Project init

---
+++ devenv.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  languages.javascript = {
+    enable = true;
+    pnpm = {
+      enable = true;
+      install = {
+          enable = true;
+        };
+    };
+  };
+
+  services.postgres = {
+    enable = true;
+    initialScript = "CREATE USER postgres SUPERUSER;";
+  };
+
+  enterShell = ''
+  echo "welcome to order  and eat app"
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
