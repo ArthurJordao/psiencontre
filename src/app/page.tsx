2b0b2c34d1ab39b3f7bf1de43418c216bf8f1fe6
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-08-18 16:14:22 -0300

    Make landing page don't scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,9 @@ import {
   SelectValue,
 } from "~/components/ui/select"
 
-export default function Home() {
+export default async function Home() {
   return (
-    <>
+    <div className="h-screen">
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-white px-4 md:px-6 shadow-md">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <h1>
@@ -52,38 +52,36 @@ export default function Home() {
           </DropdownMenu>
         </div>
       </header>
-      <main className="h-screen px-4">
-        <div className="md:pt-60 md:mx-auto md:max-w-4xl flex flex-col pt-32">
-          <h1 className="text-3xl text-center font-semibold">Encontre seu psicólogo hoje</h1>
-          <p className="mt-4 text-muted-foreground text-lg">Psiencontre é uma plataforma onde você pode encontrar um psicólogo que atenda melhor suas necessidades.</p>
-          <div className="flex flex-row flex-1 gap-4 items-baseline">
-            <Select>
-              <SelectTrigger aria-label="Selecione a abordagem desejada">
-                <SelectValue placeholder="Selectione a abordagem desejada" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Psicanalise</SelectLabel>
-                  <SelectItem value="freudiana"> Psicanálise Freudiana</SelectItem>
-                  <SelectItem value="junguiana">Psicanálise Junguiana</SelectItem>
-                  <SelectItem value="lacaniana">Psicanálise Lacaniana</SelectItem>
-                  <SelectItem value="kleiniana">Psicanálise Kleiniana</SelectItem>
-                  <SelectItem value="winnicottiana">Psicanálise Winnicottiana</SelectItem>
-                </SelectGroup>
-                <SelectGroup>
-                  <SelectLabel>Behaviorismo</SelectLabel>
-                  <SelectItem value="tcc">Terapia cognitivo comportamental (TCC)</SelectItem>
-                  <SelectItem value="comportamental">Terapia Comportamental</SelectItem>
-                  <SelectItem value="dialetica">Comportamental dialética (DBT)</SelectItem>
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-            <Button className="uppercase mt-4">Procurar</Button>
-          </div>
-          <Button asChild variant="link" className="mt-4 text-muted-foreground"><Link href="/linhas-psicologia">Gostaria de entender mais sobre as diferentes linhas</Link></Button>
-          <Button asChild variant="link" className="mt-16 text-base"><Link href="/psicologo" >Clique aqui se for psicólogo</Link></Button>
+      <main className="md:pt-60 md:mx-auto md:max-w-4xl flex flex-col pt-32  px-4">
+        <h1 className="text-3xl text-center font-semibold">Encontre seu psicólogo hoje</h1>
+        <p className="mt-4 text-muted-foreground text-lg">Psiencontre é uma plataforma onde você pode encontrar um psicólogo que atenda melhor suas necessidades.</p>
+        <div className="flex flex-row flex-1 gap-4 items-baseline">
+          <Select>
+            <SelectTrigger aria-label="Selecione a abordagem desejada">
+              <SelectValue placeholder="Selectione a abordagem desejada" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Psicanalise</SelectLabel>
+                <SelectItem value="freudiana"> Psicanálise Freudiana</SelectItem>
+                <SelectItem value="junguiana">Psicanálise Junguiana</SelectItem>
+                <SelectItem value="lacaniana">Psicanálise Lacaniana</SelectItem>
+                <SelectItem value="kleiniana">Psicanálise Kleiniana</SelectItem>
+                <SelectItem value="winnicottiana">Psicanálise Winnicottiana</SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>Behaviorismo</SelectLabel>
+                <SelectItem value="tcc">Terapia cognitivo comportamental (TCC)</SelectItem>
+                <SelectItem value="comportamental">Terapia Comportamental</SelectItem>
+                <SelectItem value="dialetica">Comportamental dialética (DBT)</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Button className="uppercase mt-4">Procurar</Button>
         </div>
+        <Button asChild variant="link" className="mt-4 text-muted-foreground"><Link href="/linhas-psicologia">Gostaria de entender mais sobre as diferentes linhas</Link></Button>
+        <Button asChild variant="link" className="mt-16 text-base"><Link href="/psicologo" >Clique aqui se for psicólogo</Link></Button>
       </main>
-    </>
+    </div>
   )
 }
