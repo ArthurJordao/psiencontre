4d3ac1d80857e6ccebddcc92d01c717e3f6ea71a
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-08-18 21:56:27 -0300

    Fix typo

--- src/app/psicologos/page.tsx
+++ src/app/psicologos/page.tsx
@@ -15,8 +15,8 @@ export default async function Home() {
       <main className="md:pt-60 md:mx-auto md:max-w-4xl flex flex-col pt-32 px-4">
         <h1 className="text-3xl text-center font-semibold">Crie sua conta hoje</h1>
         <p className="mt-4 text-muted-foreground text-lg">
-          Psiencontre é uma plataforma que conectar pacientes com psicólogos de forma 100% gratuíta.
-          Esqueça taxas para sites que ganham valor por sessão. Tenha autonomia para divulgar seu nome de graça!
+          Psiencontre é uma plataforma que conecta pacientes com psicólogos de forma 100% gratuíta.
+          Esqueça taxas para sites que ganham valor por sessão. Tenha autonomia para divulgar seu trabalho de graça!
         </p>
         <Button variant="default" className="mt-4"><Link href="/api/auth/signin">Cadastre-se</Link></Button>
       </main>
